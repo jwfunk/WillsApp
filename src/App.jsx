ebafbe2f6882d1fadeb7d6e8c5493c401b88c954
JWFunk <JWFunk@outlook.com>
2025-10-27 19:54:05 -0500

    popup on by default for adding image marker

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import {useRef, useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -12,14 +12,8 @@ import '@aws-amplify/ui-react/styles.css';
 import config from './amplifyconfiguration.json';
 import { FileUploader } from '@aws-amplify/ui-react-storage';
 import { list } from 'aws-amplify/storage';
-function LocationMarker(user) {
-  const [position, setPosition] = useState(null)
-  const map = useMapEvents({
-    click(e) {
-      setPosition(e.latlng)
-      //map.flyTo(e.latlng, map.getZoom())
-    },
-  })
+function OpenMarker({user,position}) {
+	const markerRef = useRef(null);
   const processFile = ({ file, key }) => {
   return {
     file,
@@ -29,9 +23,12 @@ function LocationMarker(user) {
     },
   };
 };
+useEffect(() => {
+	    markerRef.current.openPopup();
+  }, [position]);
 
-  return position === null ? null : (
-    <Marker position={position}>
+return (
+    <Marker position={position} ref={markerRef}>
       <Popup>
 	  <FileUploader
       acceptedFileTypes={['image/*']}
@@ -41,7 +38,19 @@ function LocationMarker(user) {
       processFile={processFile}
     />
 	  </Popup>
-    </Marker>
+	</Marker>)
+}
+function LocationMarker(user) {
+  const [position, setPosition] = useState(null)
+  const map = useMapEvents({
+    click(e) {
+      setPosition(e.latlng)
+      //map.flyTo(e.latlng, map.getZoom())
+    },
+  })
+  return position === null ? null : (
+    <OpenMarker position = {position} user = {user}>
+    </OpenMarker>
   )
 }
 function UserImages(user){
